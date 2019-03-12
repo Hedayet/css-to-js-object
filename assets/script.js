f97a5c2019d725ca812de7a2e46d1b6a77da7b9a
hemel-cse <hasibul.amin.hemel@gmail.com>
2019-03-12 16:54:05 +0600

    indentation fix

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,61 @@
 const copyToClipboard = id => {
-    const str = document.getElementById(id).value;
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-  };
+  const str = document.getElementById(id).value
+  const el = document.createElement('textarea')
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+}
 
 const convert = () => {
-    let inp = document.getElementById('inp').value;
-    inp = inp.replace(/^\s+|\s+$/g, '');
-    const re = /(style\ *=\ *)?\"*\'*([^'"]*)/;
-    const elems = re.exec(inp);
-    let stylePrefix = elems[1] ?  'style = ' : '';
-    const attrs = conquere(elems[2]);
-    let rets = [];
-    for(let k in attrs) {
-      rets.push(dashedToCamelCase(k) + ': \"' + attrs[k] + '\"');
-    }
-
-    document.getElementById('out').value = stylePrefix + "{{ " + rets.join(', ') + " }}";
-    return false;
+  let inp = document.getElementById('inp').value
+  inp = inp.replace(/^\s+|\s+$/g, '')
+  const re = /(style\ *=\ *)?\"*\'*([^'"]*)/
+  const elems = re.exec(inp)
+  let stylePrefix = elems[1] ? 'style = ' : ''
+  const attrs = conquere(elems[2])
+  let rets = []
+  for (let k in attrs) {
+    rets.push(dashedToCamelCase(k) + ': "' + attrs[k] + '"')
   }
 
-const conquere = (styleString) => {
-    let styles = {};
-    if(!styleString) {
-      return styles;
-    }
-    const attrs = styleString.split(';');
-    for(i in attrs) {
-      if (attrs.hasOwnProperty(i)) {
-        let attr = attrs[i];
-        if(!attr) continue;
-        const lst = attr.split(':');
-        styles[lst[0].trim()] = lst[1].trim();
-      }
-    }
-    return styles;
-  }
+  document.getElementById('out').value =
+    stylePrefix + '{{ ' + rets.join(', ') + ' }}'
+  return false
+}
 
-const dashedToCamelCase = (str) => {
-    let ret = '';
-    for(let i = 0; i < str.length; i++) {
-      if(str[i] == '-') {
-        if(i < str.length-1) {
-          ret += str[i+1].toUpperCase();
-          i ++;
-        }
-			  continue;
-      }
-		  ret += str[i];
+const conquere = styleString => {
+  let styles = {}
+  if (!styleString) {
+    return styles
+  }
+  const attrs = styleString.split(';')
+  for (i in attrs) {
+    if (attrs.hasOwnProperty(i)) {
+      let attr = attrs[i]
+      if (!attr) continue
+      const lst = attr.split(':')
+      styles[lst[0].trim()] = lst[1].trim()
     }
-	return ret;
+  }
+  return styles
+}
+
+const dashedToCamelCase = str => {
+  let ret = ''
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == '-') {
+      if (i < str.length - 1) {
+        ret += str[i + 1].toUpperCase()
+        i++
+      }
+      continue
+    }
+    ret += str[i]
+  }
+  return ret
 }
